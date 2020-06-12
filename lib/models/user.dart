ec595bc871278815a231555733adf1830612c330
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2020-06-12 11:47:51 +0530

    Expand user model

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -4,35 +4,54 @@ import 'package:meta/meta.dart';
 
 class User {
   final String _id;
+  final String _password;
   final String _name;
+  final bool _isAdmin;
+  final Map<String, dynamic> _details;
 
   const User({
     @required String id,
+    @required String password,
     @required String name,
+    @required bool isAdmin,
+    @required Map<String, dynamic> details,
   })  : this._id = id,
+        this._password = password,
         this._name = name,
+        this._isAdmin = isAdmin,
+        this._details = details,
         assert(id != null),
-        assert(name != null);
+        assert(password != null),
+        assert(name != null),
+        assert(isAdmin != null);
 
-  String get id {
-    return _id;
-  }
+  String get id => _id;
 
-  String get name {
-    return _name;
-  }
+  String get password => _password;
+
+  String get name => _name;
+
+  bool get isAdmin => _isAdmin;
+
+  Map<String, dynamic> get details => _details;
 
   factory User.formJson(Map<String, dynamic> map) {
     return User(
       id: map['id'],
+        password: map['password'],
       name: map['name'],
+        isAdmin: map['is_admin'],
+        details: map['details']
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
       'id': id,
+      'password': password,
       'name': name,
+      'is_admin': isAdmin,
+      'details': details
     };
   }
 
