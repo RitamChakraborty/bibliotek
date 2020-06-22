4c7dc79921eaadd67a8ee8af8bfbfb75c51e9745
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2020-06-22 17:13:41 +0530

    Complete login bloc

--- lib/bloc/login_bloc/login_bloc.dart
+++ lib/bloc/login_bloc/login_bloc.dart
@@ -3,12 +3,16 @@ import 'dart:convert';
 import 'package:bibliotek/bloc/login_bloc/login_events/login_event.dart';
 import 'package:bibliotek/bloc/login_bloc/login_states/login_state.dart';
 import 'package:bibliotek/models/user.dart';
+import 'package:bibliotek/services/firestore_services.dart';
 import 'package:bibliotek/services/shared_preferences_services.dart';
+import 'package:bibliotek/utils/Sha256.dart';
 import 'package:bloc/bloc.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class LoginBloc extends Bloc<AbstractLoginEvent, AbstractLoginState> {
   SharedPreferencesService _sharedPreferenceServices =
       SharedPreferencesService();
+  FirestoreServices _firestoreServices = FirestoreServices();
 
   @override
   AbstractLoginState get initialState => LoginInitialState();
@@ -44,8 +48,38 @@ class LoginBloc extends Bloc<AbstractLoginEvent, AbstractLoginState> {
           passwordErrorMessage: "Password can not be empty",
         );
       } else {
-        print(id);
-        print(password);
+        yield LoginLoadingState();
+
+        Stream<List<DocumentSnapshot>> userDocumentSnapshotListStream =
+            _firestoreServices.getUserDocuments(id: id);
+
+        await for (List<DocumentSnapshot> userDocumentSnapshotList
+            in userDocumentSnapshotListStream) {
+          if (userDocumentSnapshotList.isEmpty) {
+            yield LoginErrorState(
+              idErrorMessage: "ID not found",
+            );
+          } else {
+            for (DocumentSnapshot documentSnapshot
+                in userDocumentSnapshotList) {
+              Map<String, dynamic> data = documentSnapshot.data;
+              String passwordHash = Sha256().convert(string: password);
+
+              if (data['password'] == passwordHash) {
+                User user = User.fromJson(data);
+                yield LoginSuccessState(user: user);
+              } else {
+                yield LoginErrorState(
+                  passwordErrorMessage: "Password is incorrect",
+                );
+              }
+
+              break;
+            }
+          }
+
+          break;
+        }
       }
     }
 //    if (event is LoginEvent) {
