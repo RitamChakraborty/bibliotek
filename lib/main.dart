c20b0fe023fa0047e47902638a86e6b0d521f878
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2020-06-30 12:32:34 +0530

    create seperate provider to access firestore functions

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,9 @@ import 'package:bibliotek/bloc/issue_book_bloc/issue_book_bloc.dart';
 import 'package:bibliotek/bloc/login_bloc/login_bloc.dart';
 import 'package:bibliotek/bloc/submit_book_bloc/submit_book_bloc.dart';
 import 'package:bibliotek/pages/login_pages/home_page.dart';
+import 'package:bibliotek/providers/firestore_provider.dart';
 import 'package:bibliotek/providers/user_provider.dart';
+import 'package:bibliotek/services/firestore_services.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
@@ -25,8 +27,17 @@ class MyApp extends StatelessWidget {
         BlocProvider<BookBloc>.value(value: BookBloc()),
         BlocProvider<SubmitBookBloc>.value(value: SubmitBookBloc()),
       ],
-      child: ChangeNotifierProvider<UserProvider>.value(
-        value: UserProvider(user: null),
+      child: MultiProvider(
+        providers: [
+          ChangeNotifierProvider<UserProvider>.value(
+            value: UserProvider(user: null),
+          ),
+          Provider<FireStoreProvider>.value(
+            value: FireStoreProvider(
+              firestoreServices: FirestoreServices(),
+            ),
+          ),
+        ],
         child: MaterialApp(
           title: 'Bibliotek',
           theme: ThemeData(
---
+++ lib/providers/firestore_provider.dart
@@ -0,0 +1,69 @@
+import 'package:bibliotek/models/book.dart';
+import 'package:bibliotek/models/issued_book.dart';
+import 'package:bibliotek/models/subject.dart';
+import 'package:bibliotek/models/user.dart';
+import 'package:bibliotek/services/firestore_services.dart';
+import 'package:meta/meta.dart';
+
+class FireStoreProvider {
+  final FirestoreServices _firestoreServices;
+
+  const FireStoreProvider({@required FirestoreServices firestoreServices})
+      : this._firestoreServices = firestoreServices,
+        assert(firestoreServices != null);
+
+  Stream<User> getUserById({@required String refId}) =>
+      _firestoreServices.getUserById(refId: refId);
+
+  Future<User> getUserFromId({@required String id}) =>
+      _firestoreServices.getUserFromId(id: id);
+
+  Stream<List<Subject>> getSubjects() => _firestoreServices.getSubjects();
+
+  Stream<Book> getBookByRefId({@required String refId}) =>
+      _firestoreServices.getBookByRefId(refId: refId);
+
+  Future<void> changePassword(
+          {@required String refId, @required String newPassword}) =>
+      _firestoreServices.changePassword(refId: refId, newPassword: newPassword);
+
+  Future<Subject> getSubjectByName({@required String subject}) =>
+      _firestoreServices.getSubjectByName(subject: subject);
+
+  Future<void> addBook({@required Book book, @required String subject}) =>
+      _firestoreServices.addBook(book: book, subject: subject);
+
+  Future<Map<String, dynamic>> getBookExistence({@required Book book}) =>
+      _firestoreServices.getBookExistence(book: book);
+
+  Future<void> updateBookByRefId(
+          {@required String refId, @required int copies}) =>
+      _firestoreServices.updateBookByRefId(refId: refId, copies: copies);
+
+  Stream<List<User>> getStudents() => _firestoreServices.getStudents();
+
+  Stream<List<IssuedBook>> getIssuedBooks() =>
+      _firestoreServices.getIssuedBooks();
+
+  Stream<List<User>> getStudentsWithPendingBooks(
+          {@required List<dynamic> issuedBookRefs}) =>
+      _firestoreServices.getStudentsWithPendingBooks(
+          issuedBookRefs: issuedBookRefs);
+
+  Future<List<Book>> getBooksIssuedByStudent({@required User student}) =>
+      _firestoreServices.getBooksIssuedByStudent(student: student);
+
+  Future<void> issueBook({@required IssuedBook issuedBook}) =>
+      _firestoreServices.issueBook(issuedBook: issuedBook);
+
+  Future<void> submitBook({@required IssuedBook issuedBook}) =>
+      _firestoreServices.submitBook(issuedBook: issuedBook);
+
+  Stream<IssuedBook> getIssuedBookById({@required String refId}) =>
+      _firestoreServices.getIssuedBookById(refId: refId);
+
+  Future<Map<String, dynamic>> getIssuedBookAsFutureById(
+          {@required String issuedBookRef}) =>
+      _firestoreServices.getIssuedBookAsFutureById(
+          issuedBookRef: issuedBookRef);
+}
